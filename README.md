a1e78bd75212ff97ecb098096f45264a03e3bfa9
Dodi <kertesz.jozsef@simonyi.bme.hu>
2021-10-03 12:08:09 +0200

    feat: telnet gateway

---
+++ README.md
@@ -0,0 +1,3 @@
+# py-telnet
+
+Simple package to read traffic from a telnet server, and also be able to send messages.
---
+++ tnet.py
@@ -0,0 +1,38 @@
+from telnetlib import Telnet
+from threading import Thread
+
+
+def reader(tn, update_handle):
+    """Reads lines from a telnet connection, sends each received line to update_handle."""
+    while True:
+        msg = tn.read_until(b"\n", 5)
+        if msg:
+            update_handle(msg.decode())
+
+
+class TelnetGateway:
+    """Using a single telnet connection, reads updates and sends messages."""
+
+    def __init__(self, host, port):
+        self.tn = Telnet(host, port, 5)
+        self.reader_thread = Thread(target=reader, args=[self.tn, self.update_handle], daemon=True)
+        self.reader_thread.start()
+
+    def update_handle(self, msg):
+        print(f"Received update: {msg.strip()}")
+
+    def send_msg(self, msg):
+        self.tn.write(msg.encode() + b"\n")
+        print(f"Written: {msg}")
+
+
+def main():
+    gw = TelnetGateway("localhost", 1025)
+    print("hello")
+    while True:
+        m = input("What do you want to send?\n")
+        gw.send_msg(m)
+
+
+if __name__ == "__main__":
+    main()
